fd57f5355ec2d885bd0badd8d484e026beb1e1f7
samclassix <samclassix@proton.me>
2025-07-03 18:44:49 +0200

    feat: testing freeze, unfreeze, modules

--- test/TestStablecoin.ts
+++ test/TestStablecoin.ts
@@ -2,19 +2,32 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Stablecoin } from '../typechain';
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
+import { evm_increaseTime, getTimeStamp } from './helper';
+import { parseEther } from 'viem';
 
 describe('Deploy Stablecoin', function () {
 	let stable: Stablecoin;
 	let curator: SignerWithAddress;
 	let guardian: SignerWithAddress;
+	let moduleInit: SignerWithAddress;
 	let module: SignerWithAddress;
 	let user: SignerWithAddress;
 
+	const timelock = 60 * 60 * 24 * 7;
+	const expiredAt = 999999999999;
+	const publicFee = parseEther('10000');
+
 	beforeEach(async function () {
-		[curator, guardian, module, user] = await ethers.getSigners();
+		[curator, guardian, moduleInit, module, user] = await ethers.getSigners();
 
 		const Stablecoin = await ethers.getContractFactory('Stablecoin');
 		stable = await Stablecoin.deploy('USDU', 'USDU', curator.address);
+
+		await stable.connect(curator).setModule(moduleInit, expiredAt, 'moduleInit');
+		await stable.connect(moduleInit).mintModule(user, publicFee * 2n);
+
+		await stable.setTimelock(timelock);
+		await stable.setGuardian(guardian);
 	});
 
 	describe('Role Checks, Curator Role', function () {
@@ -48,6 +61,193 @@ describe('Deploy Stablecoin', function () {
 
 		it('Should revert verifyValidModule role', async function () {
 			await expect(stable.verifyValidModule(curator.address)).to.be.reverted;
+		});
+	});
+
+	describe('setFreeze', function () {
+		it('should freeze account', async function () {
+			await expect(stable.connect(curator).setFreeze(user.address, 'Suspicious activity'))
+				.to.emit(stable, 'SetFreeze')
+				.withArgs(curator.address, user.address, 'Suspicious activity');
+
+			const freezeTime = await stable.unfreeze(user.address);
+			expect(freezeTime).to.be.gt(0);
+		});
+
+		it('should revert if already frozen', async function () {
+			await stable.connect(curator).setFreeze(user.address, 'First');
+			await expect(stable.connect(curator).setFreeze(user.address, 'Second')).to.be.revertedWithCustomError(stable, 'AlreadySet');
+		});
+	});
+
+	describe('setUnfreeze', function () {
+		beforeEach(async function () {
+			await stable.connect(curator).setFreeze(user.address, 'Reason');
+		});
+
+		it('should set pending unfreeze', async function () {
+			await expect(stable.connect(guardian).setUnfreeze(user.address, 'Start unfreeze')).to.emit(stable, 'SubmitUnfreeze');
+
+			const pending = await stable.pendingUnfreeze(user.address);
+			expect(pending.validAt).to.be.gt(0);
+		});
+
+		it('should revert if not frozen', async function () {
+			await expect(stable.connect(guardian).setUnfreeze(user.address, 'Start unfreeze')).to.emit(stable, 'SubmitUnfreeze');
+			await evm_increaseTime(2 * timelock + 1000);
+
+			await stable.connect(curator).acceptUnfreeze(user.address); // unfreeze
+			await expect(stable.connect(guardian).setUnfreeze(user.address, 'Fail')).to.be.revertedWithCustomError(stable, 'AlreadySet');
+		});
+
+		it('should revert if already pending', async function () {
+			await stable.connect(guardian).setUnfreeze(user.address, 'Pending');
+			await expect(stable.connect(guardian).setUnfreeze(user.address, 'Again')).to.be.revertedWithCustomError(
+				stable,
+				'AlreadyPending'
+			);
+		});
+	});
+
+	describe('revokePendingUnfreeze', function () {
+		beforeEach(async function () {
+			await stable.connect(curator).setFreeze(user.address, 'Frozen');
+			await stable.connect(guardian).setUnfreeze(user.address, 'Start unfreeze');
+		});
+
+		it('should revoke pending unfreeze', async function () {
+			await expect(stable.connect(guardian).revokePendingUnfreeze(user.address, 'Mistake')).to.emit(stable, 'RevokeUnfreeze');
+
+			const pending = await stable.pendingUnfreeze(user.address);
+			expect(pending.validAt).to.equal(0);
+		});
+
+		it('should revert if no pending unfreeze', async function () {
+			await stable.connect(guardian).revokePendingUnfreeze(user.address, 'One');
+			await expect(stable.connect(guardian).revokePendingUnfreeze(user.address, 'Two')).to.be.revertedWithCustomError(
+				stable,
+				'NoPendingValue'
+			);
+		});
+	});
+
+	describe('acceptUnfreeze', function () {
+		beforeEach(async function () {
+			await stable.connect(curator).setFreeze(user.address, 'Frozen');
+			await stable.connect(guardian).setUnfreeze(user.address, 'Request unfreeze');
+
+			// Fast forward time
+			await ethers.provider.send('evm_increaseTime', [timelock * 2 + 1]);
+			await ethers.provider.send('evm_mine');
+		});
+
+		it('should accept unfreeze after timelock', async function () {
+			await expect(stable.connect(user).acceptUnfreeze(user.address)).to.emit(stable, 'SetUnfreeze');
+
+			const freezeTime = await stable.unfreeze(user.address);
+			expect(freezeTime).to.equal(0);
+
+			const pending = await stable.pendingUnfreeze(user.address);
+			expect(pending.validAt).to.equal(0);
+		});
+	});
+
+	describe('setModulePublic', function () {
+		it('should have fee in wallet', async function () {
+			expect(await stable.balanceOf(user)).to.be.greaterThanOrEqual(publicFee);
+		});
+
+		it('should submit module with timelock', async function () {
+			await expect(stable.connect(user).setModulePublic(module.address, expiredAt, 'Add module', publicFee))
+				.to.emit(stable, 'SubmitModule')
+				.withArgs(user.address, module.address, expiredAt, 'Add module', timelock * 2);
+
+			const pending = await stable.pendingModules(module.address);
+			expect(pending.validAt).to.be.gt(0);
+			expect(pending.value).to.equal(expiredAt);
+		});
+
+		it('should revert if already set', async function () {
+			await stable.connect(user).setModulePublic(module.address, expiredAt, 'First', publicFee);
+
+			// Fast forward time to allow acceptance
+			await ethers.provider.send('evm_increaseTime', [timelock * 2 + 1]);
+			await ethers.provider.send('evm_mine');
+
+			await stable.connect(user).acceptModule(module.address);
+
+			await expect(stable.connect(user).setModulePublic(module.address, expiredAt, 'Again', publicFee)).to.be.revertedWithCustomError(
+				stable,
+				'AlreadySet'
+			);
+		});
+
+		it('should revert if already pending', async function () {
+			await stable.connect(user).setModulePublic(module.address, expiredAt, 'First', publicFee);
+
+			await expect(
+				stable.connect(user).setModulePublic(module.address, expiredAt + 1, 'Duplicate', publicFee)
+			).to.be.revertedWithCustomError(stable, 'AlreadyPending');
+		});
+	});
+
+	describe('revokePendingModule', function () {
+		beforeEach(async function () {
+			await stable.connect(user).setModulePublic(module.address, expiredAt, 'Init', publicFee);
+		});
+
+		it('should revoke a pending module', async function () {
+			await expect(stable.connect(curator).revokePendingModule(module.address, 'Revoke it'))
+				.to.emit(stable, 'RevokePendingModule')
+				.withArgs(curator.address, module.address, 'Revoke it');
+
+			const pending = await stable.pendingModules(module.address);
+			expect(pending.validAt).to.equal(0);
+		});
+
+		it('should revert if no pending module', async function () {
+			await stable.connect(curator).revokePendingModule(module.address, 'Revoke it');
+
+			await expect(stable.connect(curator).revokePendingModule(module.address, 'Try again')).to.be.revertedWithCustomError(
+				stable,
+				'NoPendingValue'
+			);
+		});
+	});
+
+	describe('acceptModule', function () {
+		let expiredAt: number;
+
+		beforeEach(async function () {
+			expiredAt = (await getTimeStamp()) ?? 0 + timelock * 4;
+			await stable.connect(user).setModulePublic(module.address, expiredAt, 'Pending', publicFee);
+
+			await ethers.provider.send('evm_increaseTime', [timelock * 2 + 1]);
+			await ethers.provider.send('evm_mine');
+		});
+
+		it('should accept module after timelock', async function () {
+			await expect(stable.connect(user).acceptModule(module.address))
+				.to.emit(stable, 'SetModule')
+				.withArgs(user.address, module.address);
+
+			const mod = await stable.modules(module.address);
+			expect(mod).to.equal(expiredAt);
+
+			const pending = await stable.pendingModules(module.address);
+			expect(pending.validAt).to.equal(0);
+		});
+
+		it('should expire', async function () {
+			await expect(stable.connect(user).acceptModule(module.address))
+				.to.emit(stable, 'SetModule')
+				.withArgs(user.address, module.address);
+
+			await evm_increaseTime(timelock * 2 + 1000);
+
+			await expect(stable.verifyValidModule(module))
+				.to.revertedWithCustomError(stable, 'NotValidModuleRole')
+				.withArgs(module.address);
 		});
 	});
 });
